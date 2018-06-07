606028716972b3e035d76ec5cd89ebd5409e88da
chenmenghui <chenmenghui_008@163.com>
2018-06-07 10:37:36 +0800

    'PHP7'

--- calculator.php
+++ calculator.php
@@ -5,10 +5,13 @@ class calculator {
 
     /**
      * calculator constructor.
+     * @param string $init 初始值
      * @param int $scale 精确到第几位
      */
-    public function __construct($scale = 3) {
-        $this->memory = 0;
+    public function __construct(string $init = '0', int $scale = 3) {
+        $this->memory = $init;
+        $scale = $scale - 1;
+        bcscale($scale);
         $this->scale = $scale;
     }
 
@@ -17,8 +20,8 @@ class calculator {
      * @param $num
      * @return $this
      */
-    protected function math_add($num) {
-        $this->memory = bcadd($this->memory, $num, $this->scale);
+    protected function math_add(string $num) {
+        $this->memory = bcadd($this->memory, $num);
         return $this;
     }
 
@@ -27,8 +30,8 @@ class calculator {
      * @param $num
      * @return $this
      */
-    protected function math_minus($num) {
-        $this->memory = bcsub($this->memory, $num, $this->scale);
+    protected function math_minus(string $num) {
+        $this->memory = bcsub($this->memory, $num);
         return $this;
     }
 
@@ -37,8 +40,8 @@ class calculator {
      * @param $num
      * @return $this
      */
-    protected function math_multiply($num) {
-        $this->memory = bcmul($this->memory, $num, $this->scale);
+    protected function math_multiply(string $num) {
+        $this->memory = bcmul($this->memory, $num);
         return $this;
     }
 
@@ -47,8 +50,8 @@ class calculator {
      * @param $num
      * @return $this
      */
-    protected function math_divide($num) {
-        $this->memory = bcdiv($this->memory, $num, $this->scale);
+    protected function math_divide(string $num) {
+        $this->memory = bcdiv($this->memory, $num);
         return $this;
     }
 
@@ -56,9 +59,9 @@ class calculator {
      * 顺序加
      * @return $this
      */
-    function add() {
-        foreach (func_get_args() as $value) {
-            $this->math_add($value);
+    public function add(string ...$params) {
+        foreach ($params as $param) {
+            $this->math_add($param);
         }
         return $this;
     }
@@ -67,9 +70,9 @@ class calculator {
      * 顺序减
      * @return $this
      */
-    function minus() {
-        foreach (func_get_args() as $value) {
-            $this->math_minus($value);
+    public function minus(string ...$params) {
+        foreach ($params as $param) {
+            $this->math_minus($param);
         }
         return $this;
     }
@@ -78,9 +81,9 @@ class calculator {
      * 顺序乘
      * @return $this
      */
-    function multiply() {
-        foreach (func_get_args() as $value) {
-            $this->math_multiply($value);
+    public function multiply(string ...$params) {
+        foreach ($params as $param) {
+            $this->math_multiply($param);
         }
         return $this;
     }
@@ -89,34 +92,42 @@ class calculator {
      * 顺序除
      * @return $this
      */
-    function divide() {
-        foreach (func_get_args() as $value) {
-            $this->math_divide($value);
+    public function divide(string ...$params) {
+        foreach ($params as $param) {
+            $this->math_divide($param);
         }
         return $this;
     }
 
     /**
      * 等于
-     * @return int
+     * @return string
      */
-    function equal() {
+    public function equal() :string {
         return $this->memory;
     }
 
     /**
-     * 魔术变量 化为字符串
-     * @return int
+     * 四舍五入
+     * @return string
      */
-    function __toString() {
-        return $this->equal();
+    public function round() :string {
+        return round($this->memory, $this->scale - 1);
+    }
+
+    /**
+     * 魔术变量 化为字符串
+     * @return string
+     */
+    public function __toString() {
+        return $this->round();
     }
 
     /**
      * 清楚记忆
      * @return $this
      */
-    function clear() {
+    public function clear() {
         $this->memory = 0;
         return $this;
     }
